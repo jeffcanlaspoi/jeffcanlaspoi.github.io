f9d7278613c5ea15327a361be25a9acff7be1a26
jeffcanlaspoi <jeffcanlas.poi@gmail.com>
2024-03-01 16:38:41 +0800

    javascript activity 02

---
+++ activities/javascript/02/assets/scripts.js
@@ -0,0 +1,87 @@
+document.getElementById('english-grade').addEventListener('keyup', function(){
+
+    const englishGrade = parseInt(document.getElementById('english-grade').value);
+    const mathGrade = parseInt(document.getElementById('math-grade').value);
+    const dataStrucGrade = parseInt(document.getElementById('data-struc-grade').value);
+    const compProgGrade = parseInt(document.getElementById('comp-prog-grade').value);
+    const webProgGrade = parseInt(document.getElementById('web-prog-grade').value);
+    
+    const totalGrade = englishGrade + mathGrade + dataStrucGrade + compProgGrade + webProgGrade;
+
+    const averageGrade = totalGrade / 5;
+
+    document.getElementById('total-grade').value = totalGrade;
+    document.getElementById('average-grade').value = averageGrade;
+
+});
+
+document.getElementById('math-grade').addEventListener('keyup', function(){
+
+    const englishGrade = parseInt(document.getElementById('english-grade').value);
+    const mathGrade = parseInt(document.getElementById('math-grade').value);
+    const dataStrucGrade = parseInt(document.getElementById('data-struc-grade').value);
+    const compProgGrade = parseInt(document.getElementById('comp-prog-grade').value);
+    const webProgGrade = parseInt(document.getElementById('web-prog-grade').value);
+    
+    const totalGrade = englishGrade + mathGrade + dataStrucGrade + compProgGrade + webProgGrade;
+
+    const averageGrade = totalGrade / 5;
+
+    document.getElementById('total-grade').value = totalGrade;
+    document.getElementById('average-grade').value = averageGrade;
+
+});
+
+document.getElementById('data-struc-grade').addEventListener('keyup', function(){
+
+    const englishGrade = parseInt(document.getElementById('english-grade').value);
+    const mathGrade = parseInt(document.getElementById('math-grade').value);
+    const dataStrucGrade = parseInt(document.getElementById('data-struc-grade').value);
+    const compProgGrade = parseInt(document.getElementById('comp-prog-grade').value);
+    const webProgGrade = parseInt(document.getElementById('web-prog-grade').value);
+    
+    const totalGrade = englishGrade + mathGrade + dataStrucGrade + compProgGrade + webProgGrade;
+
+    const averageGrade = totalGrade / 5;
+
+    document.getElementById('total-grade').value = totalGrade;
+    document.getElementById('average-grade').value = averageGrade;
+
+});
+
+document.getElementById('comp-prog-grade').addEventListener('keyup', function(){
+
+    const englishGrade = parseInt(document.getElementById('english-grade').value);
+    const mathGrade = parseInt(document.getElementById('math-grade').value);
+    const dataStrucGrade = parseInt(document.getElementById('data-struc-grade').value);
+    const compProgGrade = parseInt(document.getElementById('comp-prog-grade').value);
+    const webProgGrade = parseInt(document.getElementById('web-prog-grade').value);
+    
+    const totalGrade = englishGrade + mathGrade + dataStrucGrade + compProgGrade + webProgGrade;
+
+    const averageGrade = totalGrade / 5;
+
+    document.getElementById('total-grade').value = totalGrade;
+    document.getElementById('average-grade').value = averageGrade;
+
+});
+
+document.getElementById('web-prog-grade').addEventListener('keyup', function(){
+
+    const englishGrade = parseInt(document.getElementById('english-grade').value);
+    const mathGrade = parseInt(document.getElementById('math-grade').value);
+    const dataStrucGrade = parseInt(document.getElementById('data-struc-grade').value);
+    const compProgGrade = parseInt(document.getElementById('comp-prog-grade').value);
+    const webProgGrade = parseInt(document.getElementById('web-prog-grade').value);
+    
+    const totalGrade = englishGrade + mathGrade + dataStrucGrade + compProgGrade + webProgGrade;
+
+    const averageGrade = totalGrade / 5;
+
+    document.getElementById('total-grade').value = totalGrade;
+    document.getElementById('average-grade').value = averageGrade;
+
+});
+
+
+
