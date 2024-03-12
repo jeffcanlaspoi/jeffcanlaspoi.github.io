92fd59c65ea45bc5dac99b9f7720c4afb48dc225
jeffcanlaspoi <jeffcanlas.poi@gmail.com>
2024-03-12 13:26:31 +0800

    loop statement activity

---
+++ activities/javascript/loop_activity/assets/script.js
@@ -0,0 +1,24 @@
+let btnExecute = document.getElementById("btn-execute");
+
+btnExecute.addEventListener('click', function() {
+
+    let numberOfRows = document.getElementById("input-number-rows").value;
+    let numberOfColumns = document.getElementById("input-number-columns").value;
+    let tblBodyHTML = "";
+    let counter = 1;
+
+    for (let rowCount = 1; rowCount <= numberOfRows; rowCount++) {
+        
+        let tblRowHTML = "<tr>";
+
+        for (let columnCount = 1; columnCount <= numberOfColumns; columnCount++) {
+            tblRowHTML += "<td class='text-center'>" + counter + "</td>";
+            counter++;
+        }
+
+        tblRowHTML += "</tr>";
+        tblBodyHTML += tblRowHTML;
+    }
+    
+    document.getElementById("tbl-body").innerHTML = tblBodyHTML;
+});
