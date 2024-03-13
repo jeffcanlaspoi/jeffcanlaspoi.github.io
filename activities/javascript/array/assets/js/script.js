3396a811d46ccfaba6b41e78693d2a2fd6b2b845
jeffcanlaspoi <jeffcanlas.poi@gmail.com>
2024-03-13 23:43:14 +0800

    add total hours

--- activities/javascript/array/assets/js/script.js
+++ activities/javascript/array/assets/js/script.js
@@ -5,6 +5,7 @@ document.getElementById('btn-login').addEventListener('click', function(){
     let employeeId = document.getElementById('input-employee-id').value;
     let employeeName = document.getElementById('input-employee-name').value;
     let workingHours = document.getElementById('input-working-hours').value;
+    let total = 0;
 
     employeeList.push( [employeeId, employeeName, workingHours] );
     DisplayRow();
@@ -17,7 +18,8 @@ function Remove(index){
 
 function DisplayRow(){
     let tblDataHTML = "";
-    
+    let total = 0;
+
     for(let rowCount = 0; rowCount < employeeList.length; rowCount++){
         tblDataHTML += "<tr class='text-center'>";
         tblDataHTML += "<td class='p-2'>" + employeeList[rowCount][0] + "</td>"; 
@@ -25,7 +27,10 @@ function DisplayRow(){
         tblDataHTML += "<td>" + employeeList[rowCount][2] + "</td>"; 
         tblDataHTML += "<td><button class='btn btn-danger btn-sm' onclick='Remove(" + rowCount + ")'> Remove </button></td>"; 
         tblDataHTML += "</tr>";
+
+        total = total + parseFloat(employeeList[rowCount][2]);
     }
    
     document.getElementById('tbl-body').innerHTML = tblDataHTML;
+    document.getElementById('total-hrs').innerText = total;
 }
