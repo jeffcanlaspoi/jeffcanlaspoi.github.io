0f446c0bbbd7ba44675f4d97e702467cf8a405e5
jeffcanlaspoi <jeffcanlas.poi@gmail.com>
2024-03-14 20:57:35 +0800

    edit function

--- activities/javascript/array_activity/assets/js/script.js
+++ activities/javascript/array_activity/assets/js/script.js
@@ -5,13 +5,34 @@ document.getElementById('btn-login').addEventListener('click', function(){
     let employeeId = document.getElementById('input-employee-id').value;
     let employeeName = document.getElementById('input-employee-name').value;
     let workingHours = document.getElementById('input-working-hours').value;
-    let total = 0;
+    let existing = false;
 
-    employeeList.push( [employeeId, employeeName, workingHours] );
+    for(let i = 0; i < employeeList.length; i++){
+        if(employeeList[i][0] === employeeId){
+            employeeList[i][1] = employeeName;
+            employeeList[i][2] = workingHours;
+            existing = true;
+        }
+
+    }
+
+    if(!existing){
+        employeeList.push( [employeeId, employeeName, workingHours] );
+    }
+
     DisplayRow();
+    document.getElementById('input-employee-id').value = "";
+    document.getElementById('input-employee-name').value = "";
+    document.getElementById('input-working-hours').value = "";
 });
 
-function Remove(index){
+function edit(index){
+    document.getElementById('input-employee-id').value = employeeList[index][0];
+    document.getElementById('input-employee-name').value = employeeList[index][1];
+    document.getElementById('input-working-hours').value = employeeList[index][2];
+}
+
+function remove(index){
     employeeList.splice(index, 1);
     DisplayRow()
 }
@@ -25,7 +46,10 @@ function DisplayRow(){
         tblDataHTML += "<td class='p-2'>" + employeeList[rowCount][0] + "</td>"; 
         tblDataHTML += "<td>" + employeeList[rowCount][1] + "</td>"; 
         tblDataHTML += "<td>" + employeeList[rowCount][2] + "</td>"; 
-        tblDataHTML += "<td><button class='btn btn-danger btn-sm' onclick='Remove(" + rowCount + ")'> Remove </button></td>"; 
+        tblDataHTML += "<td>"
+        tblDataHTML += "<button class='btn btn-danger btn-sm' onclick='remove(" + rowCount + ")'> Remove </button>"; 
+        tblDataHTML += "<button class='btn btn-warning btn-sm' onclick='edit(" + rowCount + ")'> Edit </button>"; 
+        tblDataHTML += "</td>"; 
         tblDataHTML += "</tr>";
 
         total = total + parseFloat(employeeList[rowCount][2]);
