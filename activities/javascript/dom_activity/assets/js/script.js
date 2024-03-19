bcb0639fcaabe023e2803070b0b436d768391ea8
jeffcanlaspoi <jeffcanlas.poi@gmail.com>
2024-03-19 13:46:01 +0800

    grading system activity

---
+++ activities/javascript/dom_activity/assets/js/script.js
@@ -0,0 +1,34 @@
+// Add function
+document.querySelector('#btnAdd').addEventListener('click', function(){
+    let inputGrade = document.createElement("input");
+    inputGrade.className = "form-control mb-2";
+    inputGrade.type="number";
+    inputGrade.placeholder="Enter Grade";
+
+    let gradeField = document.querySelector('#input-grade-field');
+    gradeField.append(inputGrade);
+});
+
+// Calculate function
+document.querySelector('#btnCalculate').addEventListener('click', function(){
+    average();
+});
+
+// Remove function
+document.querySelector('#btnRemove').addEventListener('click', function(){
+    document.querySelector("input:last-child").remove();
+    average();
+});
+
+// Calculation
+function average(){
+    let inputs = document.querySelectorAll('input');
+    let gradeResult = 0;
+
+    for(let input of inputs){
+        gradeResult += Number(input.value);
+    }
+
+    let average = gradeResult / inputs.length;
+    document.querySelector("#grade-result").innerText = average;
+}
