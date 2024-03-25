8713dba4c6dbfa0917fe6ff7374ed3b02b43777b
jeffcanlaspoi <jeffcanlas.poi@gmail.com>
2024-03-25 22:24:52 +0800

    fix

--- javascript/js_cc/assets/js/04_loops_assignment.js
+++ javascript/js_cc/assets/js/04_loops_assignment.js
@@ -1,11 +1,14 @@
-let startNum = parseInt(prompt("Enter start Number"));
-let endNum = parseInt(prompt("Enter end number"));
+let startNum = parseInt(prompt("Enter MIN"));
+let endNum = parseInt(prompt("Enter MAX"));
 
 const isEven = (number) => {
   return number % 2 === 0;
 }
 
-for(let i = startNum; i <= endNum; i++){
-  isEven(i) ? console.log(i) : null;
-}
+if(startNum > endNum)
+  console.log('Invalid input, MIN input is larger than MAX input');
+else
+  for(let i = startNum; i <= endNum; i++){
+    isEven(i) ? console.log(i) : null;
+  }
   
