0bb5a89eeb18591f2222067665c7467efec04441
jeffcanlaspoi <jeffcanlas.poi@gmail.com>
2024-04-01 20:41:30 +0800

    onclick event:

--- javascript/js_cc/assets/js/06_array_activity.js
+++ javascript/js_cc/assets/js/06_array_activity.js
@@ -22,3 +22,25 @@ const {odd: resultOdd, even: resultEven } = reslutInput;
 
 console.log("Even Result:", resultEven);
 console.log("Odd Result:", resultOdd);
+
+// const acceptInput = (odd, even) => {
+//   const userInput = parseFloat(prompt("Enter number, press 0 to stop:"));
+
+//   if(userInput === 0){
+//     return { odd, even };
+//   } 
+
+//   if(userInput % 2 === 0){
+//     even.push(userInput);
+//   } else {
+//     odd.push(userInput);
+//   }
+
+//   return acceptInput(odd, even);
+// }
+
+// const reslutInput = acceptInput(odd, even);
+// const {odd: resultOdd, even: resultEven } = reslutInput;
+
+// console.log("Even Result:", resultEven);
+// console.log("Odd Result:", resultOdd);
